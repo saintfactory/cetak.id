3494df6b74f1ce0e0a2dd4d600a095ba2c844d4a
alvinisasi <azraqazravin@gmail.com>
2019-05-13 16:49:31 +0700

    Add tachyons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Header />
     <HelloWorld msg="Welcome to Your Vue.js App on Now 2.0"/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 import HelloWorld from './components/HelloWorld.vue'
-
+import '../node_modules/vue-tachyons/dist/index.css'
+import '../node_modules/vue-tachyons/dist/vue-tachyons.js'
 export default {
   name: 'app',
   components: {
+    Header,
     HelloWorld
   }
 }
